fcca3777b794dabc3623217fd71e2c5369fa0030
trien <triien@hotmail.fr>
2018-08-29 17:57:41 +0200

    first working version

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Yo-ho-ho');
+var fs = require('fs');
+var translations = require('./translations.json');
+var sourceFile = 'source.css';
+var fileToWrite = 'test.css';
+fs.readFile(sourceFile, 'utf8', function (err,data) {
+	if (err) {
+		return console.log(err);
+	}
+
+	for (var find in translations) {
+		let replace = translations[find];
+		data = data.replace(new RegExp(find,'g'), replace);
+	};
+
+	fs.writeFile(fileToWrite, data, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+});
